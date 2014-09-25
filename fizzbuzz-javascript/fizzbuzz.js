ff900f55037db2b74a9eb98c41f3fd596d247ed8
Andy010 <andy_gates_010@hotmail.co.uk>
2014-09-25 11:42:28 +0100

    First commit & added code

---
+++ fizzbuzz-javascript/fizzbuzz.js
@@ -0,0 +1,16 @@
+function Fizzbuzz(){}
+
+Fizzbuzz.prototype.isDivisibleByThree = function(number) {
+	= function(number){
+		return number % 3 === 0;
+};
+
+Fizzbuzz.prototype.isDivisibleByFive = function(number) {
+	= function(number){
+	return number % 5 === 0;
+};
+
+Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
+	function(number){
+	return number % 15 == 0;
+};
